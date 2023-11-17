bef75c59be453520cd693701478d0646e918a5a8
이트루 <leetrue@iteuluui-MacBookPro.local>
2023-11-18 00:27:24 +0900

    setting. styled-components globalstyle provider 작성, reset css 설치

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
+// import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-
+import { GlobalStyleProvider } from "./lib/styled-components/GlobalStyleProvider.tsx";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
+      <GlobalStyleProvider />
       <App />
     </BrowserRouter>
   </React.StrictMode>
